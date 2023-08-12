fdad9fb3d2a66c8abc07e96bc31ca70f2faf93d3
Sundar <109157207+Sundar-600@users.noreply.github.com>
2023-08-12 09:52:57 +0530

    Updated Calculator.c
    Comments added

--- Calculator.c
+++ Calculator.c
@@ -20,27 +20,32 @@ typedef struct node
 
 //Functions
 
-//Essentials
-int validator(char *);
-float Pow(float ,int );
-int Priority(char );
+//Essentials 
+
+int validator(char *); //Check the correctness of the user's input
+int Priority(char );//To return the priority value to the operands [^,/,*,+,-]
+float Pow(float ,int );//For some reason the pow() function from math.h isn't properly linked while compiling on my linux
 
 //Conversion Operations
-Node* Array_Linked(char *);
-Node* Infix_Postfix(Node*,Node *);
+
+Node* Array_Linked(char *);//It converts array values into linked list
+Node* Infix_Postfix(Node*,Node *);//It converts the linklist into the postfix version of linked list for evaluation
 
 //Linked-list Operations
-Node* Push(Node *,char ,float );
+
+Node* Push(Node *,char ,float );//for inserting the numbers or operators inside the linked list
 
 
 //Postfix stack Operations
-void push_char(char *,int *,char );
-char pop_char(char *,int *);
+
+void push_char(char *,int *,char ); //used to insert operator into the stack (This function helps Infix_Postfix() to do the work) 
+char pop_char(char *,int *); //used to take out the operator from the stack (This function helps Infix_Postfix() to do the work) 
 
 //Evaluation Operations
-float Evaluation(Node* );
-void push_float(float *,float,int *);
-float pop_float(float *,int *);
+
+float Evaluation(Node* );//Evaluate the postfix and gives final answer
+void push_float(float *,float,int *);//used to insert numbers into float stack (This function helps Evaluation() to do the work)
+float pop_float(float *,int *);//used to take out the numbers from the float stack (This function helps Evaluation() to do the work)
 
 
 int main()
@@ -62,12 +67,12 @@ int main()
 			case 1 :
 				printf("\nEnter the Mathematical Expression : \n");
 				scanf("%s",input);
-				valid = validator(input);
+				valid = validator(input);//Validate the expression
 				if(valid == 1)
 				{
-					infix = Array_Linked(input);
-					postfix = Infix_Postfix(infix, postfix);
-					printf("Answer : %.3f",Evaluation(postfix));
+					infix = Array_Linked(input);//Converting input to linked list
+					postfix = Infix_Postfix(infix, postfix);//Converting infix to postfix
+					printf("Answer : %.3f",Evaluation(postfix));//Evaluating and Displaying the answer
 					infix = NULL;
 					postfix = NULL;
 				}
@@ -91,7 +96,16 @@ int main()
 	return 0;
 }
 
+/*
+Input  : Node pointer
+Return : float
 
+Description :  
+    It Push the numbers one by one from postfix to float stack until it reached an operator 
+    Then it pops two numbers from the float stack in the order of op2 and op1 and do the operations
+    and push the resultant in the float stack again until it reaches the end of the linked list 
+    Then it returns the final floating number
+*/
 float Evaluation(Node* postfix)
 {
 	float result = 0;
@@ -145,10 +159,22 @@ float Evaluation(Node* postfix)
 		}
 	}
 	result = pop_float(stack,&top);
+	free(stack);
 	return result;
 
 }
 
+/*
+Input  : Infix Node pointer contains value and empty Postfix Node pointer
+Return : postfix Node pointer
+
+Description :
+    It takes the the infix pointer and traverse through it node by node until it reaches the end
+    if the node->symbol contains '_' it Push the number into the postfix linked list
+    Else Based on the priority of the symbol decide to push directly to the char stack 
+    Or pop some values already inside the character stack to push it to the postfix linked list
+    Once the infix reaches end pop all the values inside the stack and push it to postfix and return it
+*/
 Node* Infix_Postfix(Node *infix,Node *postfix)
 {
 	char *stack = (char *)calloc(MIN,sizeof(char));
@@ -207,10 +233,25 @@ Node* Infix_Postfix(Node *infix,Node *postfix)
 		}
 
 	}
+	free(stack);
 	return postfix;
 }
 
+/*
+Input  : Character Array pointer
+Return : Node pointer
 
+Description :
+    It travese through the character array until it reaches '\0'
+    If it encouters the operators [(,),^,*,/,+,-] it push it to the infix linked list
+    Else it store the number values in a small temp char array and converts it into actual floating number
+	and push it into the infix linked list
+	
+	Array            Linked List
+	+-+-+-+-+-+      +-----+  +---+   +---+
+	|4|1|9|+|1|  --> |419|_|->|0|+|-->|1|_|
+	+-+-+-+-+-+      +-----+  +---+   +---+
+*/
 Node* Array_Linked(char *input)
 {
 	int count = 0,k = 0,m = 0,change = 1;
@@ -337,7 +378,15 @@ Node* Array_Linked(char *input)
 }
 
 
-
+/*
+Input  : floating number and integer number
+Return : floating number
+  
+Description :
+    If your math.h works perfectly Include math.h header and remove this function 
+    Both definition and declaration and rename every occurance of Pow() into pow() 
+	It works perfectly fine
+*/
 float Pow(float base,int power)
 {
 	float ans = 1;
@@ -359,6 +408,14 @@ float Pow(float base,int power)
 	}
 }
 
+/*
+Input  : character array pointer ,integer pointer and character value
+Return : nothing
+
+Description :
+	Checks the overflow condition 
+	Store the parameter value symbol in the stack_ptr at the index of the tos(top of stack) and increment it
+*/
 void push_char(char *stack_ptr,int *tos,char symbol)
 {
 	if(*tos >= (MIN-1))
@@ -372,6 +429,14 @@ void push_char(char *stack_ptr,int *tos,char symbol)
 	}
 }
 
+/*
+Input  : character array pointer ,integer pointer
+Return : character
+
+Description :
+	Checks the underflow condition 
+	Take value from the stack_ptr at the index of the tos(top of stack) and decrement it and return the taken value
+*/
 char pop_char(char *stack_ptr,int *tos)
 {
 	char symbol;
@@ -388,6 +453,14 @@ char pop_char(char *stack_ptr,int *tos)
 
 }
 
+/*
+Input  : float array pointer ,float value and integer pointer
+Return : nothing
+
+Description :
+	Checks the overflow condition 
+	Store the parameter value num in the stack at the index of the top and increment it
+*/
 void push_float(float* stack,float num,int *top)
 {
 	if((*top) <= (MIN-1))
@@ -396,6 +469,14 @@ void push_float(float* stack,float num,int *top)
 	}
 }
 
+/*
+Input  : float array pointer ,integer pointer 
+Return : nothing
+
+Description :
+	Checks the underflow condition 
+	Take the value from the stack at the index of the top and decrement it and return the taken value
+*/
 float pop_float(float* stack,int *top)
 {
 	float num;
@@ -412,6 +493,14 @@ float pop_float(float* stack,int *top)
 	}
 }
 
+/*
+Input  : Node pointer ,character value and floating value
+Return : Node pointer
+
+Description :
+	Create a Node and assign values for node->symbol and node->number with the parameter values and make node->next NULL
+	Join the current node with the parameter node->next and return the final node pointer
+*/
 Node* Push(Node *first,char symbol,float number)
 {
 	Node *temp = (Node *)malloc(sizeof(Node));
@@ -434,6 +523,13 @@ Node* Push(Node *first,char symbol,float number)
 	return first;
 }
 
+/*
+Input  : character value
+Return : integer value
+
+Description :
+	According to the priority we given it returns the integer value
+*/
 int Priority(char s)
 {
 	switch(s)
@@ -454,6 +550,15 @@ int Priority(char s)
 	}
 }
 
+/*
+Input  : character array pointer
+Return : integer
+
+Description :
+	It Check the equality of () "Brackets" finding invalid like ((2+3) , 3*6)
+	Then it checks the wrongly typed expressions like 66++3 ,-4- etc..
+	If it finds anything wrong in the input expression it returns -1 
+*/
 int validator(char *ptr)
 {
 	int count = 0;
